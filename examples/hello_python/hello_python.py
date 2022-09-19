f730bbaae2c1cb298818e6fc4eaa7c9664c65f9f
Jesús González <jesusgm@vmware.com>
2022-09-19 16:17:42 +0200

    fix: Simplifying examples and adding new ones

--- examples/hello_python/hello_python.py
+++ examples/hello_python/hello_python.py
@@ -1,20 +1,9 @@
-from datetime import datetime
-import sys
 import os
 
-print("Content-Type: text/plain")
+print("Content-Type: text/plain;")
 print("")
-
-# Datetime and Sys
-print("*** INFO ***")
-now = datetime.now()
-dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-print("Date and time:", dt_string)
-print("sys.platform:", sys.platform)
-print()
 
 # Env Vars
 print("*** ENV VARS ***")
 for k, v in sorted(os.environ.items()):
     print(k+':', v)
-
