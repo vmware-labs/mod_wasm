a0109384f7d9a97044b968859d0a84de75c207d0
Asen Alexandrov <alexandrov@vmware.com>
2022-09-29 17:22:37 +0200

    Fix minor typo issues with the examples

--- examples/wasm_modules/python-scripts/cgi_search_word_count.py
+++ examples/wasm_modules/python-scripts/cgi_search_word_count.py
@@ -2,21 +2,36 @@
 
 import os, sys
 
+print("Content-Type: text/plain")
+print("")
+
+def print_available_files():
+	current_dir = "."
+	print(f"Available files at '{current_dir}':")
+	try:
+		dirs = os.listdir(current_dir)
+		for entry in dirs:
+			print(f" - {entry}")
+	except Exception as e:
+		print("ERROR! " + str(e))
+
 FILE = os.getenv('HTTP_FILE')
 WORD = os.getenv('HTTP_WORD')
 
 if FILE == None or WORD == None:
 	print("ERROR! Set FILE and WORD headers!", file=sys.stderr)
+	print("ERROR! Set FILE and WORD headers!")
+	print_available_files()
 	sys.exit()
 
 try:
 	file = open(FILE, "r")
 except Exception as e:
 	print(f"ERROR! Couldn't open file {FILE}! {e}", file=sys.stderr)
+	print(f"ERROR! Couldn't open file {FILE}! {e}")
+	print_available_files()
 
 read_data = file.read()
 word_count = read_data.count(WORD)
 
-print("Content-Type: text/plain")
-print("")
 print(f"The word '{WORD}' appeared {word_count} times.")
